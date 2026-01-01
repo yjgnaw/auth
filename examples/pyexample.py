ded48f9d73047a6c6f115a742a2a620cb462e1b7
AlfredChester <fredbao1126@gmail.com>
2026-01-01 15:14:40 +0800

    fix: fixed pyexample.py

--- examples/pyexample.py
+++ examples/pyexample.py
@@ -1,11 +1,13 @@
 import hmac
 import hashlib
 import requests
+import random
 
 
 class KeyVerifier:
-    SERVER_URL = "https://example.com/"
+    SERVER_URL = "https://dev-auth.fredbao1126.workers.dev/"
     SERVER_KEY = "ncrfQY9J6Id2VzhTlTdrXBHsGXM4jTLZkj7mBGAks3s"
+    SALT_LENGTH = 32
 
     def calculate_hash(key: str, salt: str) -> str:
         key_bytes = key.encode("utf-8")
@@ -15,17 +17,27 @@ class KeyVerifier:
 
         return signature
 
-    def verify_key(key: str, salt: str, version: str) -> bool:
+    def verify_key(key: str, version: str) -> bool:
+        salt = "".join(
+            [
+                random.choice(
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+                )
+                for _ in range(KeyVerifier.SALT_LENGTH)
+            ]
+        )
         key_hash = KeyVerifier.calculate_hash(key, salt)
         # Send request to server to verify key
-        response = requests.post(
+        # /check?val=key_hash&salt=salt&version=version
+        response = requests.get(
             KeyVerifier.SERVER_URL + "check",
-            json={
-                "key": key_hash,
+            params={
+                "val": key_hash,
                 "salt": salt,
                 "version": version,
             },
         )
+        print(response.url)
         if response.status_code != 200:
             return False
         server_hash = KeyVerifier.calculate_hash(KeyVerifier.SERVER_KEY, salt)
@@ -34,3 +46,12 @@ class KeyVerifier:
                 "Server response hash does not match expected value. server may be compromised."
             )
         return True
+
+
+print(KeyVerifier.verify_key("my_secret_key_123", "1.0.0"))  # False
+print(
+    KeyVerifier.verify_key("VBmoQbL5jZ6YRjYGS4gbX095rYinyKdf85OPdUR4P0R", "1.0.0")
+)  # True
+print(
+    KeyVerifier.verify_key("VBmoQbL5jZ6YRjYGS4gbX095rYinyKdf85OPdUR4P0R", "2.0.0")
+)  # False
